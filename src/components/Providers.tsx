e4f190756df1f870f9829f9e0d378c1f0e21fff1
Gabriel Hogan <Gabriel.T.Hogan@gmail.com>
2023-10-23 07:17:35 -0400

    chore: remove residual toast duration

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -10,7 +10,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
       <QueryClientProvider client={queryClient}>
-        <Toaster richColors closeButton duration={999999} />
+        <Toaster richColors closeButton />
 
         {children}
       </QueryClientProvider>
