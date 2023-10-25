7cccb59634c4cf51dd93e565458c60fabe0edc15
Gabriel Hogan <Gabriel.T.Hogan@gmail.com>
2023-10-25 10:12:10 -0400

    feat: backend change

--- src/app/backend/page.tsx
+++ src/app/backend/page.tsx
@@ -1,17 +1,27 @@
 import { db } from "@/lib/db";
 import { vote } from "@/lib/db/schema";
+import { getTableColumns } from "drizzle-orm";
 import { FC } from "react";
 
 interface PageProps {}
 
+// oh yeah, this is the future
+export const runtime = "edge";
+
 const Page: FC<PageProps> = async ({}) => {
-  const results = await db.select().from(vote);
+  const { email, updated_at, ...otherCols } = getTableColumns(vote);
+
+  const results = await db.select(otherCols).from(vote);
 
   return (
-    <div>
-      <pre>
-        <code>{JSON.stringify(results, null, 2)}</code>
-      </pre>
+    <div className="space-y-2">
+      [
+      {results.map((res) => (
+        <pre key={res.id} className="bg-black rounded-md text-white w-fit p-2">
+          {JSON.stringify(res, null, 2)},
+        </pre>
+      ))}
+      ]
     </div>
   );
 };
