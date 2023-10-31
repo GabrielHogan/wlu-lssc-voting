3ab8496181b79dda540c2efd271d55d40bf0339b
Gabriel Hogan <Gabriel.T.Hogan@gmail.com>
2023-10-30 20:12:29 -0400

    chore: start branding changes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -8,14 +8,14 @@ interface FooterProps {}
 const Footer: FC<FooterProps> = ({}) => {
   return (
     <>
-      <div className="p-2 border-t border-muted bg-background fixed bottom-0 right-0 left-0 flex items-center justify-between z-50">
-        <div className="flex items-center ">
+      <div className="p-2 border-t border-muted bg-background fixed bottom-0 right-0 left-0 flex items-center justify-between">
+        <div className="flex items-center">
           <p className="text-xs font-light ">Powered by </p>
           <Link className="flex items-center" href="https://gabrielhogan.com">
             <Icon className="mr-0 h-8 w-8 rounded-md text-blue-950 dark:text-white" />
             <h1 className="text-lg font-bold">
               <span className="text-blue-950 dark:text-white">Glacier</span>
-              <span className="text-blue-500">Edu</span>
+              <span className="text-emerald-500">Learn</span>
             </h1>
           </Link>
         </div>
